6a800ab7f85a24adb9387f292289a37ea222188d
Raulcezarsantos <raulcezarhaha@gmail.com>
2021-11-19 23:19:20 -0300

    Atividade de Funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,147 @@
+// 1. Leia o código abaixo
+    
+    // function minhaFuncao(variavel) {
+    // 	return variavel * 5
+    // }
+    
+//     console.log(minhaFuncao(2))
+//     console.log(minhaFuncao(10))
+    
+//     a) O que vai ser impresso no console?
+    // Multiplicação de 2x5 e 10x5
+
+//     b) O que aconteceria se retirasse os dois `console.log` e simplesmente invocasse a função `minhaFuncao(2)` e `minhaFuncao(10)`? O que apareceria no console?
+    // no console ficaria em branco, pois não teria sido chamado nehuma função para mostrar o resultado nele
+
+
+    // 2. Leia o código abaixo
+    
+    
+    // let textoDoUsuario = prompt("Insira um texto");
+
+    // const outraFuncao = function(texto) {
+    //     return texto.toLowerCase().includes("cenoura")
+    // }
+    
+    // const resposta = outraFuncao(textoDoUsuario)
+    // console.log(resposta)
+    
+    
+    // a. Explique o que essa função faz e qual é sua utilidade
+      // Ela pede um texto ao usuario,e verifica se tem a palavra cenoura
+      
+    // b. Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+    //      i.   `Eu gosto de cenoura` True
+    //      ii.  `CENOURA é bom pra vista` True
+    //      iii. `Cenouras crescem na terra` False
+
+
+    // 1. Escreva as funções explicadas abaixo:
+    
+    // a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como: 
+    
+    // const testefuncao = function(resposta) {
+    //     return resposta
+    // }
+    
+    // const resposta = "oi, meu nome é raul, tenho 24 anos,moro em Paraupebas, sou estudante"
+    // console.log(resposta)
+
+    // b)  Agora escreva uma função que receba 4 parâmetros que correspondem às informações de uma pessoa: o nome (`string`), a idade (`number`), a cidade (`string`) e uma profissão (`string`). Ela deve retornar uma `string` que unifique todas as informações da pessoa em uma só mensagem com o template:
+    
+
+    //     const testandoTrocaNomes = function() {   
+
+    //    console.log("oi meu nome é", nome,"tenho",idade,"anos","moro na cidade de",endereco,"minha profissão é",profissao )    
+    //    }
+       
+    //    const nome = "Raul"
+    //    const idade = "24"
+    //    const endereco = "parauapebas"
+    //    const profissao = "estudante" 
+
+    //    testandoTrocaNomes (nome,idade,endereco,profissao)
+
+    // 2. Escreva as funções explicadas abaixo:
+    
+    // a) Escreva uma função que receba 2 números como parâmetros, e, dentro da função, faça a soma das duas entradas e retorne o resultado. Invoque a função e imprima no console o resultado.
+        //   const vamosSomar = function (num1, num2) {
+        //       return num1 + num2
+
+        //   }
+         
+        //   console.log(vamosSomar(4,5))
+
+    // b) Faça uma função que recebe 2 números e retorne um booleano que informa se o primeiro número é **maior ou igual** ao segundo.
+    //   const vamosDeBoleano = function (num1, num2){
+    //    return num1 >= num2
+
+    //   }
+    //   console.log(vamosDeBoleano(5,5))
+
+    // c) Escreva uma função que receba um número e devolva um booleano indicando se ele é par ou não
+        //  const testeImparPar = function(num1){
+        //  return num1%2 === 0
+         
+        //  }
+        //  console.log(testeImparPar(2))
+
+
+
+    // d) Faça uma função que recebe uma mensagem (`string`) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma versão dela em letras maiúsculas.
+     
+    //    const vamosSrtring = function(frase){
+    //     return frase = "eita"
+
+
+    //    }
+
+
+
+      
+    //    console.log(vamosSrtring().toUpperCase().length)
+    //    console.log(vamosSrtring().toUpperCase())
+
+    // 3. Crie uma função para cada uma das operações básicas (soma, subtração, multiplicação e divisão). Em seguida, peça para o usuário inserir dois números e **chame** essas 4 funções com esses valores inputados pelo usuário sendo o argumento. Por fim, mostre no console o resultado das operações:
+    
+
+    // Números inseridos: 30 e 3
+    // Soma: 33
+    
+    
+    // const soma = function(usuario1,usuario2){
+    // return  usuario1 + usuario2 
+
+
+    // }
+   
+    // console.log(soma(5,7))
+
+    // // // Diferença: 27
+    // // const dif = function(usuario1,usuario2){
+    // //     return  usuario1 % usuario2 
+    
+    
+    // //     }
+        
+    // //     console.log(dif(71, 7))
+
+    // // // Multiplicação: 90
+    // // const mul = function(usuario1,usuario2){
+    // //     return  usuario1 * usuario2 
+    
+    
+    // //     }
+        
+    // //     console.log(mul(71, 7))
+    // // // Divisão: 10
+
+    // // const divis = function(usuario1,usuario2){
+    // //     return  usuario1 / usuario2 
+    
+    
+    // //     }
+        
+    // //     console.log(divis(71, 7))
+    
+    
