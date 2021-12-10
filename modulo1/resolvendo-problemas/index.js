1e8dae8e2373f3dde890055540ffeae006996fbc
Raulcezarsantos <raulcezarhaha@gmail.com>
2021-12-10 18:49:48 -0300

    Resumo Escrito 7 passos

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,9 @@
+//  Faça um resumo sobre quais são os 7 passos fundamentais para a resolução de problemas e como eles podem nos ajudar a solucionar nossos desafios de programação.
+
+// 1: ter bastante atenção no enunciado, a maioria dos exercicios são faceis mais podem ser torna uma muralha se não houver uma interpretação dos mesmos
+// 2: sempre observar informações relevantes dentro dos enunciados para avaliar a forma que o codigo será escrito
+// 3: sempre que iniciar o codigo imaginar uma entrada e saida no codigo
+// 4: fazer um pequeno esquema visual sobre como será feito o codigo, falar em voz alta ajuda
+// 5: escrever o codigo por parte, comece com o que voce sabe, e depois vamos lingando as informações até o codigo fazer sentindo 
+// 6:  sempre testar as mudanças dentro do codigo
+// 7: codar pensando em forma que o codigo nao fique travado apenas a uma condicao, e implantar o codigo bonito
