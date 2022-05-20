507cf75caa1fb2d63bb9fc6c249a616ee07bbc18
Raulcezarsantos <raulcezarhaha@gmail.com>
2022-05-19 21:36:36 -0300

    Entrega Completa node

---
+++ modulo4/node-package.json/Exercicio 2/index.js
@@ -0,0 +1,9 @@
+//    const num1 = Number(process.argv[2])
+//    const num2 = Number(process.argv[3])
+
+//    const soma = num1 + num2
+//    const sub = num1 - num2
+//    const mult = num1 * num2
+//    const div = num1 / num2
+
+//    console.log(`Se você somar ${num1} mais o ${num2} o resultado será ${soma}`)
