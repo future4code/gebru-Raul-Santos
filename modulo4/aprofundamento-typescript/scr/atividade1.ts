c4e69ee3162d49b20e751dc7a4e9f3c246e6ad4d
Raulcezarsantos <raulcezarhaha@gmail.com>
2022-05-26 20:57:31 -0300

    Entrega Aprofundamento Typescript

---
+++ modulo4/aprofundamento-typescript/scr/atividade1.ts
@@ -0,0 +1,35 @@
+//1°)
+
+
+// A) const minhaString:string = "Atribuindo valor"
+
+// B) const meuNumero:string | number = "45"
+
+
+
+
+// C) enum CoresArco {
+
+//     AZUL = "Azul",
+//     VERMELHO = "Vermelho",
+//     LARANJA = "Laranja",
+//     VERDE = "Verde",
+//     VIOLETA = "Violeta"
+
+// }
+
+// D) const pessoa = {
+
+//     nome: "Raul",
+//     idade: 25,
+//     corFavorita: CoresArco.VERDE
+
+// }
+
+// type pessoa = {
+//     nome:string
+//     idade:number
+//     corFavorita: string
+// }
+
+// console.log(pessoa)
---
+++ modulo4/aprofundamento-typescript/scr/atividade3.ts
@@ -0,0 +1,40 @@
+// const posts = [
+//     {
+//       autor: "Alvo Dumbledore",
+//       texto: "Não vale a pena viver sonhando e se esquecer de viver"
+//     },
+//     {
+//       autor: "Severo Snape",
+//       texto: "Menos 10 pontos para Grifinória!"
+//     },
+//     {
+//       autor: "Hermione Granger",
+//       texto: "É levi-ô-sa, não levio-sá!"
+//     },
+//     {
+//       autor: "Dobby",
+//       texto: "Dobby é um elfo livre!"
+//     },
+//     {
+//       autor: "Lord Voldemort",
+//       texto: "Avada Kedavra!"
+//     }
+//   ]
+
+//   type usuario = { 
+//     name: string, 
+//     age: number
+//     }
+    
+//     const conta: usuario = {
+//     name: "Raul",
+//     age: 25
+//     }
+
+//     function buscarPostsPorAutor(posts:[], autorInformado:string) {
+//         return posts.filter(
+//           (post:any) => {
+//             return post.autor === autorInformado
+//           }
+//         )
+//       }
