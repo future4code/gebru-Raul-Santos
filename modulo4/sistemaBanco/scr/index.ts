92b4b2032857539447a7d5ca4f689f4efe68d05f
Raulcezarsantos <raulcezarhaha@gmail.com>
2022-06-13 09:18:16 -0300

    Entrega(Andamento)

---
+++ modulo4/sistemaBanco/scr/index.ts
@@ -0,0 +1,17 @@
+import app from "./servidor"
+import {bancoDados, Clientes,Operação, Transaction} from "./base"
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+
+app.get("/users", (req: Request, res: Response) => {
+  let errorCode: number = 500;
+  try {
+    res.status(200).send(bancoDados);
+  } catch (error: any) {
+    if (errorCode === 500) {
+      res.status(errorCode).send({ message: "Erro na busca" });
+    }
+    res.status(errorCode).send({ message: error.message });
+  }
+});
